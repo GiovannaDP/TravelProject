ca8d842223d205420f3498935a70572ceddf48fb
GiovannaDP <giovannadanellipau@gmail.com>
2023-03-03 02:17:09 -0300

    criacao da tela inicial de vendas -> SEM ANUNCIOS

---
+++ DanellisTravel/Extensions/UIButton+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIButton+Extension.swift
+//  DanellisTravel
+//
+//  Created by Giovanna Danelli Pau on 03/03/23.
+//
+
+import Foundation
--- DanellisTravel/TelaInicialVendas/TelaInicialVendasController.swift
+++ DanellisTravel/TelaInicialVendas/TelaInicialVendasController.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import UIKit
+
+class TelaInicialVendasController: UIViewController {
+    
+    private var customView: TelaInicialVendasView? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        buildView()
+    }
+    
+    func buildView() {
+        view = TelaInicialVendasView()
+        customView = view as? TelaInicialVendasView
+    }
+}
