63e59c0e88ed1a60d8ea9c8289e106e0bed6d4db
GiovannaDP <giovannadanellipau@gmail.com>
2023-03-05 00:01:11 -0300

    extension uiButton

--- DanellisTravel/Extensions/UIButton+Extension.swift
+++ DanellisTravel/Extensions/UIButton+Extension.swift
@@ -5,4 +5,18 @@
 //  Created by Giovanna Danelli Pau on 03/03/23.
 //
 
-import Foundation
+import UIKit
+
+extension UIButton {
+    func underline() {
+        if let textString = self.titleLabel?.text {
+
+            let attributedString = NSMutableAttributedString(string: textString)
+            attributedString.addAttribute(NSAttributedString.Key.underlineStyle,
+                                          value: NSUnderlineStyle.single.rawValue,
+                                          range: NSRange(location: 0, length: textString.count))
+            self.setAttributedTitle(attributedString, for: .normal)
+        }
+
+    }
+}
